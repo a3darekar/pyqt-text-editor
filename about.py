1e6322a181c83b42609b2aaa8b6a2ca8eaa4868c
knowhere1998 <darekar.amey@gmail.com>
2019-09-24 12:23:22 +0530

    Added title to About Dialog

--- about.py
+++ about.py
@@ -6,17 +6,20 @@ from PyQt5.QtWidgets import QLabel, QDialogButtonBox, QDialog, QVBoxLayout
 
 
 class AboutDialog(QDialog):
+	title = "Text Editor"
+	dev_info = "Developed by Knowhere1998 (Amey Darekar)"
+
 	def __init__(self, *args, **kwargs):
 		super(AboutDialog, self).__init__(*args, **kwargs)
-
-		quitButton = QDialogButtonBox.Ok  # No cancel
+		self.setWindowTitle("About %s" % self.title)
+		quitButton = QDialogButtonBox.Ok
 		self.buttonBox = QDialogButtonBox(quitButton)
 		self.buttonBox.accepted.connect(self.accept)
 		self.buttonBox.rejected.connect(self.reject)
 
 		layout = QVBoxLayout()
 
-		title = QLabel("Text Editor")
+		title = QLabel(self.title)
 		font = title.font()
 		font.setPointSize(20)
 		title.setFont(font)
@@ -27,7 +30,7 @@ class AboutDialog(QDialog):
 		logo.setPixmap(QPixmap(os.path.join('icons', 'ma-icon-128.png')))
 		layout.addWidget(logo)
 
-		layout.addWidget(QLabel("Developed by Knowhere1998 (Amey Darekar)"))
+		layout.addWidget(QLabel(self.dev_info))
 
 		for i in range(0, layout.count()):
 			layout.itemAt(i).setAlignment(Qt.AlignHCenter)
